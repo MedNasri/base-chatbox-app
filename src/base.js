4371f65b4b1ef74d0ad7813ecc77f0e3e2a04dc3
MedNasri <62633186+MedNasri@users.noreply.github.com>
2020-08-27 01:39:29 +0100

    Initial commit

---
+++ src/base.js
@@ -0,0 +1,14 @@
+// import Rebase from 're-base'
+// import firebase from 'firebase/app'
+// import 'firebase/database'
+
+// const firebaseApp = firebase.initializeApp({
+//     apiKey: 'AIzaSyDeANe2MOBRi2tEen4rgXqleXUPNUkCW3U',
+//     authDomain: 'chatbox-app-d5dfd.firebaseapp.com',
+//     databaseURL: 'https://chatbox-app-d5dfd.firebaseio.com'
+// })
+// const base = Rebase.createClass(firebaseApp.database())
+
+
+// export { firebaseApp }
+// export default base
---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Message = ({pseudo,message}) => {
+  return (
+    <p className='user-message'>
+      {message}
+    </p>
+
+
+
+  )
+}
+
+export default Message
